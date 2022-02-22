bb116cd05bcfdaec843b181470e9efd527577743
5-Jeremy <jcarl023@ucr.edu>
2022-02-21 17:51:31 -0800

    Rewrite Linear Actuator Control Code (1st attempt)

--- RPR_linear_actuator.h
+++ RPR_linear_actuator.h
@@ -1,15 +1,34 @@
 #ifndef RPR_LINEAR_ACTUATOR_H_
 #define RPR_LINEAR_ACTUATOR_H_
 
-// Stepper motor wires should be in this order (starting furthest from the capacitor): blue, red, black, green
-#define STEP_PIN 5
-#define DIR_PIN 4
-#define HOMING_SWITCH_PIN 7
+//UART used to communicate with the Arduino Uno controlling the linear actuator
+#define LA_CNTRL_SERIAL Serial2
+
+/*** Incoming messages ***/
+// When the Uno is sending the current position of the linear actuator to the OpenCR, it sends this byte first
+#define CURR_POSITION_MSG 0x01
+// In homing mode, when the new zero position for the linear actuator is established this message is sent automatically to the OpenCR
+#define HOMING_COMPLETE_MSG 0x03
+
+/*** Outgoing messages ***/
+// The first byte in a message determines what information is being requested or will be sent. The upper nibble of this byte is all
+  // ones. If the OpenCR is sending information, it is sent 7 bits at a time with the MSB being zero
+// This message is sent in order to set the goal position for the linear actuator
+#define SET_GOAL_POSITION_MSG 0xF1
+// This message is sent in order to set the goal velocity for the linear actuator in block place mode (given in terms of the microseconds per pulse) 
+#define SET_GOAL_VELOCITY_MSG 0xF2
+// This message is sent in order to request the current position of the linear actuator in steps
+#define GET_CURR_POSITION_MSG 0xF3
+// These messages are sent to set the mode of the linear actuator
+#define SET_MODE_DEFAULT_MSG 0xF4
+#define SET_MODE_BLOCK_PLACE_MSG 0xF5
+#define SET_MODE_HOMING_MSG 0xF6
+
 #define STEP_SIZE_MM 0.01
 #define FULL_STROKE_STEPS 10000
 // Do not allow the linear actuator to get within this many steps from its limits
-//#define SAFETY_BUFFER_STEPS 100
-#define SAFETY_BUFFER_STEPS 0 // Disabled for testing
+#define SAFETY_BUFFER_STEPS 100
+//#define SAFETY_BUFFER_STEPS 0 // For testing
 //#define LA_MIN_PULSE_WIDTH 350
 #define LA_MIN_PULSE_WIDTH 200
 #define LA_MAX_PULSE_WIDTH 1000
@@ -22,20 +41,21 @@
                 // the linear actuator fully retracted
 unsigned int LA_curr_position;
 unsigned int LA_goal_position;
-unsigned int LA_accel_rate;
-unsigned int LA_curr_pulse_width;
-unsigned int LA_goal_pulse_width;
-int LA_move_dir; // 1 for forwards, -1 for reverse
-bool LA_is_accelerating;
-bool LA_is_decelerating;
+// Add variables for storing velocity in mm per second
+// unsigned int LA_accel_rate;
+// unsigned int LA_curr_pulse_width;
+// unsigned int LA_goal_pulse_width;
+// int LA_move_dir; // 1 for forwards, -1 for reverse
+// bool LA_is_accelerating;
+// bool LA_is_decelerating;
 bool block_place_mode;
-bool step_pin_state;
+bool LA_is_moving{false}; // flag to tell if the task has completed
+bool LA_is_homing;
+// bool step_pin_state;
 void Init_Linear_Actuator();
-void LA_Extend();
-void LA_Retract();
+void LA_Update_Curr_Position();
+void LA_Send_Goal_Position(unsigned int goal_position);
 bool LA_Is_Valid_Position(unsigned int pos);
-void LA_Toggle_Step_Pin();
-void LA_Set_Dir(unsigned int goal_position);
 void LA_Set_Accel_Parameters(float vel_in_mm_per_sec);
 float LA_Pos_mm();
 unsigned int LA_Vel_To_Pulse_Width(float vel_in_mm_per_sec);
@@ -55,8 +75,6 @@ struct LA_Move_Command {
 void Lin_Act_Move_Callback();
 void Prepare_LA_Move_Task(LA_Move_Command LA_cmd);
 void LA_Update_Pulse_Width();
-bool LA_is_moving{false}; // flag to tell if the task has completed
-bool LA_is_homing;
 void Homing_Callback();
 void Start_LA_Homing();
 
--- RPR_manipulator_driver.hpp
+++ RPR_manipulator_driver.hpp
@@ -39,32 +39,6 @@
 //  Arm_Move_Queue.enqueue(Move_1);
 //}
 
-// Note: All functionality has been moved to the tb3_builder_core setup function,
-/* void setup() {
-  Serial.begin(115200);
-  ////////////////////////// Base servo setup
-  Init_Base_Servo();
-  ////////////////////////// Linear actuator setup
-  Init_Linear_Actuator();
-  ////////////////////////// End servo setup
-  Init_End_Servo();
-  /////////////////////////////////////////////////
-  while(!Serial.available());
-  Serial.read();
-  Serial.println("Begin");
-
-  //Queue_Simple_Move_To_Position(240, 40);
-  //Queue_Pick_Up_Block(250, 10);
-  //Queue_Simple_Move_To_Position(220, 40);
-
-  Arm_Move_Scheduler.addTask(T_Base_Servo);
-  Arm_Move_Scheduler.addTask(T_Lin_Act);
-  Arm_Move_Scheduler.addTask(T_End_Servo);
-  Arm_Move_Scheduler.addTask(T_Homing);
-  //Start_Next_Motion();
-  Start_LA_Homing();
-} */
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*********************************** Base servo functions ****************************************************/
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -78,7 +52,7 @@ void Init_Base_Servo() {
   base_move_dir = 1;
   base_is_moving = false;
 }
-//
+
 void Base_Servo_Move_Callback() {
   // The base servo position needs to be updated as the base servo arrives at the position that was calculated 
     // in the previous iteration. When this function is first called, the base servo will be arriving at the position which
@@ -130,119 +104,135 @@ void Prepare_Base_Servo_Move_Task(int goal_position)
 float Base_PWM_to_Rad(int PWM_us) {
   return (((float)PWM_us - BASE_ZERO_POSITION_PULSE_WIDTH)/10)*DEG_TO_RAD;
 }
-//float Base_Rad_to_PWM(float radians) {
-//  return (int)(radians*RAD_TO_DEG)*10 + BASE_ZERO_POSITION_PULSE_WIDTH;
-//}
-//bool Base_Is_Valid_Position(int position) {
-//  return position >= BASE_MIN_PULSE_WIDTH && position <= BASE_MAX_PULSE_WIDTH;
-//}
-//void Update_Base_Angle_Tan(int new_pos) {
-//  // The tangent calculation is based on the average of the starting angle and the final angle for this
-//      // step in the base servo's movement
-//    int pwm_avg = (new_pos + base_curr_position)/2;
-//    base_angle_tan = fabs(tan(Base_PWM_to_Rad(pwm_avg)));
-//}
+float Base_Rad_to_PWM(float radians) {
+ return (int)(radians*RAD_TO_DEG)*10 + BASE_ZERO_POSITION_PULSE_WIDTH;
+}
+bool Base_Is_Valid_Position(int position) {
+ return position >= BASE_MIN_PULSE_WIDTH && position <= BASE_MAX_PULSE_WIDTH;
+}
+void Update_Base_Angle_Tan(int new_pos) {
+ // The tangent calculation is based on the average of the starting angle and the final angle for this
+     // step in the base servo's movement
+   int pwm_avg = (new_pos + base_curr_position)/2;
+   base_angle_tan = fabs(tan(Base_PWM_to_Rad(pwm_avg)));
+}
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///*********************************** Linear Actuator functions ***********************************************/
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//void Init_Linear_Actuator() {
-//  pinMode(DIR_PIN, OUTPUT);
-//  pinMode(STEP_PIN, OUTPUT);
-//  // Mode initialization (Full step)
-//  digitalWrite(DIR_PIN, LOW);
-//  digitalWrite(STEP_PIN, LOW);
-//  pinMode(HOMING_SWITCH_PIN, INPUT);
-//  LA_Extend();
-//  LA_curr_position = 0;
-//  LA_curr_pulse_width = LA_ACCEL_START_PULSE_WIDTH;
-//  LA_move_dir = 1;
-//  step_pin_state = LOW;
-//  LA_is_moving = false; // flag to tell if the task has completed
-//  LA_is_homing = false;
-//  LA_is_accelerating = false;
-//  LA_is_decelerating = false;
-//  block_place_mode = false;
-//}
-//
-//void Start_LA_Homing() {
-//  if (base_is_moving || LA_is_moving || end_is_moving) {
-//    Serial.println("Could not execute homing, another motion task is currently in progress");
-//    return;
-//  }
-//  LA_Retract();
-//  LA_is_homing = true;
-//  LA_is_moving = true;
-//  LA_curr_pulse_width = LA_HOMING_PULSE_WIDTH;
-//}
-//void Homing_Callback() {
-//  // Keep moving the linear actuator backwards until it trips the homing switch
-//  // We do not track steps here because the final position that the linear actuator
-//    // ends up in will be considered the new zero position
-//  // When the homing switch is not pressed, the pin connected to the OpenCR is 
-//    // connected to ground. When the switch is pressed, it is pulled up to 5V
-//  if (digitalRead(HOMING_SWITCH_PIN)) {
-//    LA_curr_position = 0; 
-//    LA_is_homing = false;
-//    LA_is_moving = false;
-//  }
-//  else {
-//    LA_Toggle_Step_Pin();
-//  }
-//}
-//
-//void Lin_Act_Move_Callback() {
-//  // Check for out-of-bounds motion
-//  if (!LA_Is_Valid_Position(LA_curr_position)) {
-//    Serial.println("Linear actuator limit reached");
-//    LA_is_moving = false;
-//  }
-//  // Check if goal has been reached
-//  if (LA_curr_position == LA_goal_position) {
-//    digitalWrite(STEP_PIN, LOW); // Since a pulse is in progress, set the pin low before stopping
-//    LA_is_moving = false;
-//    return;
-//  }
-//  LA_Update_Pulse_Width();
-//  LA_Toggle_Step_Pin();
-//  if (step_pin_state == HIGH) {
-//    LA_curr_position += LA_move_dir;
-//  }
-//}
-//void Prepare_LA_Move_Task(LA_Move_Command LA_cmd) {
-// unsigned int goal_position = LA_cmd.goal_position;
-// if (!LA_Is_Valid_Position(goal_position)) {
-//   Serial.println("goal_position was invalid in call to Prepare_LA_Move_Task");
-//   return;
-// }
-// if (LA_cmd.move_mode == HOMING_MODE) {
-//   Start_LA_Homing();
-//   return;
-// }
-// LA_goal_position = goal_position;
-// // May need to add velocity bounds check
-// LA_Set_Dir(goal_position);
-// if (LA_cmd.move_mode == BLOCK_PLACE_MODE) {
-//   block_place_mode = true;
-//   float vel_to_match = BASE_DEFAULT_VEL * (L0 + LA_Pos_mm()) * base_angle_tan;
-//   LA_curr_pulse_width = LA_Vel_To_Pulse_Width(vel_to_match);
-// }
-// else {
-//   LA_Set_Accel_Parameters(LA_cmd.vel_in_mm_per_sec);
-//   block_place_mode = false;
-// }
-// LA_is_moving = true;
-//}
-//void LA_Set_Accel_Parameters(float vel_in_mm_per_sec) {
-//  LA_is_accelerating = false; // Will be set true if the LA needs to accelerate
-//  if (vel_in_mm_per_sec > LA_MAX_VEL_NO_ACCEL) {
-//    LA_is_accelerating = true;
-//    LA_goal_pulse_width = (unsigned int)((1.0/vel_in_mm_per_sec)*5000 + 0.5);
-//    LA_curr_pulse_width = LA_ACCEL_START_PULSE_WIDTH;
-//  }
-//  else {
-//    LA_curr_pulse_width = (unsigned int)((1.0/vel_in_mm_per_sec)*5000 + 0.5);
-//  }
-//}
+void Init_Linear_Actuator() {
+ LA_CNTRL_SERIAL.begin(115200);
+ LA_curr_position = 0;
+ LA_goal_position = 0;
+ LA_is_moving = false;
+ LA_is_homing = false;
+ block_place_mode = false;
+}
+
+void Start_LA_Homing() {
+ if (base_is_moving || LA_is_moving || end_is_moving) {
+   Serial.println("Could not execute homing, another motion task is currently in progress");
+ }
+ LA_is_homing = true;
+ LA_is_moving = true;
+ LA_CNTRL_SERIAL.write(SET_MODE_HOMING_MSG);
+}
+
+void Lin_Act_Move_Callback() {
+  // If homing is in progress, just wait for the HOMING_COMPLETE message
+  // Otherwise, read any CURR_POSITION messages received from the Uno
+    // Keep only the most recent CURR_POSITION message
+    // If the current position matches the goal position, set is_moving false
+    // If a homing complete message is received, set curr_position to zero and is_moving/is_homing to false
+  while(LA_CNTRL_SERIAL.available()) {
+    // When the linear actuator is homing, the position is not being monitored so ignore all messages
+      // except for HOMING_COMPLETE_MSG
+    if (LA_is_homing) {
+      if (LA_CNTRL_SERIAL.read() == HOMING_COMPLETE_MSG) {
+        LA_curr_position = 0; 
+        LA_is_homing = false;
+        LA_is_moving = false;
+        LA_CNTRL_SERIAL.flush();
+      }
+    }
+    else {
+      if (LA_CNTRL_SERIAL.peek() == CURR_POSITION_MSG) {
+        // Only read the message now if all of the needed data has been received
+        if (LA_CNTRL_SERIAL.available() >= 3) {
+          // Get position info, and if it matches the goal position then the motion is complete
+            // After every motion is completed, flush the serial buffer
+          LA_CNTRL_SERIAL.read();
+          LA_Update_Curr_Position();
+          if (LA_curr_position == LA_goal_position) {
+            LA_is_moving = false;
+            LA_CNTRL_SERIAL.flush();
+          }
+        }
+      }
+      else {
+        // The Uno should not send any messages that do not start with the CURR_POSITION_MSG
+          // byte in this situation, but if it does just ignore them
+        LA_CNTRL_SERIAL.read();
+      }
+    }
+  }
+}
+
+void Prepare_LA_Move_Task(LA_Move_Command LA_cmd) {
+  unsigned int goal_position = LA_cmd.goal_position;
+  if (!LA_Is_Valid_Position(goal_position)) {
+    Serial.println("goal_position was invalid in call to Prepare_LA_Move_Task");
+    return;
+  }
+  LA_is_moving = true;
+  switch(LA_cmd.move_mode) {
+    case DEFAULT_MODE:
+      LA_CNTRL_SERIAL.write(SET_MODE_DEFAULT_MSG);
+      break;
+    case BLOCK_PLACE_MODE:
+      // Todo: send mode change message and then setup the goal velocity
+      break;
+    case HOMING_MODE:
+      Start_LA_Homing();
+      return;
+      break;
+    default: break;
+  }
+  LA_goal_position = goal_position;
+  LA_Send_Goal_Position(goal_position);
+  // if (LA_cmd.move_mode == BLOCK_PLACE_MODE) {
+  //   block_place_mode = true;
+  //   float vel_to_match = BASE_DEFAULT_VEL * (L0 + LA_Pos_mm()) * base_angle_tan;
+  //   // Send goal velocity command
+  //   //LA_curr_pulse_width = LA_Vel_To_Pulse_Width(vel_to_match);
+  // }
+  // else {
+  //   //LA_Set_Accel_Parameters(LA_cmd.vel_in_mm_per_sec);
+  //   block_place_mode = false;
+  // }
+}
+
+// It is assumed that the next two bytes on the serial buffer store the current position information
+void LA_Update_Curr_Position() {
+  byte lower = LA_CNTRL_SERIAL.read();
+  byte upper = LA_CNTRL_SERIAL.read();
+  unsigned int position = ((unsigned int)upper << 8) | lower;
+  // Reject any reported position that should not be possible
+  if ((position < 0) || (position > FULL_STROKE_STEPS)) {
+    return;
+  }
+  else {
+    LA_curr_position = position;
+  }
+}
+
+void LA_Send_Goal_Position(unsigned int goal_position) {
+  LA_CNTRL_SERIAL.write(SET_GOAL_POSITION_MSG);
+  byte goal_position_lower_7 = goal_position & 0x7F;
+  byte goal_position_upper_7 = (goal_position & 0x7F80) >> 7;
+  LA_CNTRL_SERIAL.write(goal_position_lower_7);
+  LA_CNTRL_SERIAL.write(goal_position_upper_7);
+}
+
 //void LA_Update_Pulse_Width() {
 //  // In order to cut down on unnecessary calculations, the velocity to match is only calculated on every 100 executions
 //    // of this function; the velocity is still updated on every execution however
@@ -288,36 +278,19 @@ float Base_PWM_to_Rad(int PWM_us) {
 //    LA_curr_pulse_width = LA_MAX_PULSE_WIDTH;
 //  }
 //}
-//float LA_Pos_mm() {
-//  return (float)LA_curr_position * 0.01;
-//}
-//void LA_Set_Dir(unsigned int goal_position) {
-//  if (goal_position >= LA_curr_position) {
-//    LA_Extend();
-//  }
-//  else {
-//    LA_Retract();
-//  }
-//}
-//void LA_Toggle_Step_Pin() {
-//  step_pin_state = step_pin_state ? LOW : HIGH;
-//  digitalWrite(STEP_PIN, step_pin_state);
-//}
-//bool LA_Is_Valid_Position(unsigned int pos) {
-//  return (pos < (FULL_STROKE_STEPS - SAFETY_BUFFER_STEPS)) && (pos >= SAFETY_BUFFER_STEPS);
-//}
-//void LA_Extend() {
-//  digitalWrite(DIR_PIN, HIGH);
-//  LA_move_dir = 1;
-//}
-//void LA_Retract() {
-//  digitalWrite(DIR_PIN, LOW);
-//  LA_move_dir = -1;
-//}
-//unsigned int LA_Vel_To_Pulse_Width(float vel_in_mm_per_sec) {
-//  return (1.0/vel_in_mm_per_sec)*5000 + 0.5;
-//}
-//
+
+float LA_Pos_mm() {
+ return (float)LA_curr_position * 0.01;
+}
+
+bool LA_Is_Valid_Position(unsigned int pos) {
+ return (pos < (FULL_STROKE_STEPS - SAFETY_BUFFER_STEPS)) && (pos >= SAFETY_BUFFER_STEPS);
+}
+
+unsigned int LA_Vel_To_Pulse_Width(float vel_in_mm_per_sec) {
+ return (1.0/vel_in_mm_per_sec)*5000 + 0.5;
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///*********************************** End servo functions ****************************************************/
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
